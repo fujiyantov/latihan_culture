f4568f3e5b0b4b9047a0c27c5ff564df91256ead
pujiyanto <puji.yantov@gmail.com>
2022-09-09 05:29:18 +0700

    update

--- js/app.js
+++ js/app.js
@@ -2,8 +2,8 @@ let startedData;
 getData();
 
 async function getData() {
-    const url = "https://dm.fujiyantov.id/api/v2/collections"
-    // const url = "https://cms.borobudurside.com/api/v2/collections"
+    // const url = "https://dm.fujiyantov.id/api/v2/collections"
+    const url = "https://cms.borobudurside.com/api/v2/collections"
     const response = await fetch(url)
     if (response) {
 
